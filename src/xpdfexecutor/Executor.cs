83d99000f93cbd30785d092da0c764381073bd58
Krishnan Kuppaswamy <krishnan.kuppaswamy@moodys.com>
2018-10-15 19:19:56 +0530

    chmod, sudo etc.,

--- src/xpdfexecutor/Executor.cs
+++ src/xpdfexecutor/Executor.cs
@@ -7,6 +7,27 @@ namespace xpdfexecutor
 {
     public class Executor
     {
+        public static void Exec(string cmd)
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\""
+                }
+            };
+
+            process.Start();
+            process.WaitForExit();
+        }
+
         public string Execute()
         {
             var executingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
@@ -20,6 +41,12 @@ namespace xpdfexecutor
 
             if (!File.Exists(fileName))
                 throw new Exception($"FileName: {fileName}, FullPath: {Path.GetFullPath(fileName)}");
+
+            try
+            {
+                Exec($"chmod 644 {fileName}");
+            }
+            catch { }
 
             var processStartInfo = new ProcessStartInfo(fileName, arguments);
             processStartInfo.RedirectStandardError = true;
@@ -35,5 +62,31 @@ namespace xpdfexecutor
             var pdfContent = File.ReadAllText(@".\test.txt");
             return pdfContent;
         }
+
+        //public string GetVersionInfo()
+        //{
+        //    var executingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        //    Trace.WriteLine($"executingDirectory: {executingDirectory}");
+
+        //    var fileName = Path.Combine(executingDirectory, "lib", "pdftotext.exe");
+        //    Trace.WriteLine($"FileName: {fileName}, FullPath: {Path.GetFullPath(fileName)}");
+
+        //    if (!File.Exists(fileName))
+        //        throw new Exception($"FileName: {fileName}, FullPath: {Path.GetFullPath(fileName)}");
+
+        //    var processStartInfo = new ProcessStartInfo(fileName, "-v");
+        //    processStartInfo.RedirectStandardError = true;
+        //    processStartInfo.UseShellExecute = false;
+        //    processStartInfo.CreateNoWindow = true;
+        //    processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+        //    Process process = Process.Start(processStartInfo);
+        //    string error = process.StandardError.ReadToEnd();
+        //    process.WaitForExit();
+        //    if (process.ExitCode != 0)
+        //        throw new Exception(error);
+
+        //    var pdfContent = File.ReadAllText(@".\vtest.txt");
+        //    return pdfContent;
+        //}
     }
 }
--- test/xpdfexecutor.UnitTest/xpdfexecutorTest.cs
+++ test/xpdfexecutor.UnitTest/xpdfexecutorTest.cs
@@ -4,6 +4,16 @@ namespace xpdfexecutor.UnitTest
 {
     public class xpdfexecutorTest
     {
+        //[Fact]
+        //public void ShouldReturnXpdfVersionInfo()
+        //{
+        //    var xpdfexecutor = new Executor();
+
+        //    var actual = xpdfexecutor.GetVersionInfo();
+
+        //    Assert.StartsWith("pdftotext version 4.00", actual);
+        //}
+
         [Fact]
         public void ShouldReturnPdfContent()
         {
