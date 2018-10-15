f64404ae65907ba8f927c1d5cbc4b4e5f77b21bf
Krishnan Kuppaswamy <krishnan.kuppaswamy@moodys.com>
2018-10-15 17:58:01 +0530

    xpdfexecutor and unit test project committed

---
+++ src/xpdfexecutor/Executor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace xpdfexecutor
+{
+    public class Executor
+    {
+        public string Execute()
+        {
+            var fileName = @".\lib\pdftotext.exe";
+            var arguments = @"-simple -f 1 "".\doc\test.pdf"" ""test.txt""";
+
+            var processStartInfo = new ProcessStartInfo(fileName, arguments);
+            processStartInfo.RedirectStandardError = true;
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.CreateNoWindow = true;
+            processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            Process process = Process.Start(processStartInfo);
+            string error = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+            if (process.ExitCode != 0)
+                throw new Exception(error);
+
+            var pdfContent = File.ReadAllText(@".\test.txt");
+            return pdfContent;
+        }
+    }
+}
---
+++ test/xpdfexecutor.UnitTest/xpdfexecutorTest.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace xpdfexecutor.UnitTest
+{
+    public class xpdfexecutorTest
+    {
+        [Fact]
+        public void ShouldReturnPdfContent()
+        {
+            var xpdfexecutor = new Executor();
+
+            var actual = xpdfexecutor.Execute();
+
+            Assert.Equal("This is first line!\r\n\f", actual);
+        }
+    }
+}
