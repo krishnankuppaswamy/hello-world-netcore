9464b2cbb83a9cca4d849be71254157f963173f5
Krishnan Kuppaswamy <krishnan.kuppaswamy@moodys.com>
2018-10-16 13:21:12 +0530

    Using XpdfNet NuGet package which would work for multi-platform!

--- src/xpdfexecutor/Executor.cs
+++ src/xpdfexecutor/Executor.cs
@@ -7,92 +7,13 @@ namespace xpdfexecutor
 {
     public class Executor
     {
-        public static void Exec(string cmd)
-        {
-            var escapedArgs = cmd.Replace("\"", "\\\"");
-
-            var process = new Process
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    RedirectStandardOutput = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true,
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                    FileName = "/bin/bash",
-                    Arguments = $"-c \"{escapedArgs}\""
-                }
-            };
-
-            process.Start();
-            process.WaitForExit();
-        }
-
         public string Execute()
         {
+            var xpdfNet = new XpdfNet.XpdfHelper();
             var executingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             Trace.WriteLine($"executingDirectory: {executingDirectory}");
-
-            var fileName = Path.Combine(executingDirectory, "lib", "pdftotext.exe");
             var argumentsPath = Path.Combine(executingDirectory, "doc", "test.pdf");
-            var arguments = $@"-simple -f 1 ""{argumentsPath}"" ""test.txt""";
-
-            Trace.WriteLine($"FileName: {fileName}, FullPath: {Path.GetFullPath(fileName)}");
-
-            if (!File.Exists(fileName))
-                throw new Exception($"FileName: {fileName}, FullPath: {Path.GetFullPath(fileName)}");
-
-            try
-            {
-                Exec($"chmod 644 {fileName}");
-            }
-            catch { }
-
-            try
-            {
-                Exec($"chown -R $USER:$USER {fileName}");
-            }
-            catch { }
-
-            var processStartInfo = new ProcessStartInfo(fileName, arguments);
-            processStartInfo.RedirectStandardError = true;
-            processStartInfo.UseShellExecute = false;
-            processStartInfo.CreateNoWindow = true;
-            processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            Process process = Process.Start(processStartInfo);
-            string error = process.StandardError.ReadToEnd();
-            process.WaitForExit();
-            if (process.ExitCode != 0)
-                throw new Exception(error);
-
-            var pdfContent = File.ReadAllText(@".\test.txt");
-            return pdfContent;
+            return xpdfNet.ToText(argumentsPath);
         }
-
-        //public string GetVersionInfo()
-        //{
-        //    var executingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        //    Trace.WriteLine($"executingDirectory: {executingDirectory}");
-
-        //    var fileName = Path.Combine(executingDirectory, "lib", "pdftotext.exe");
-        //    Trace.WriteLine($"FileName: {fileName}, FullPath: {Path.GetFullPath(fileName)}");
-
-        //    if (!File.Exists(fileName))
-        //        throw new Exception($"FileName: {fileName}, FullPath: {Path.GetFullPath(fileName)}");
-
-        //    var processStartInfo = new ProcessStartInfo(fileName, "-v");
-        //    processStartInfo.RedirectStandardError = true;
-        //    processStartInfo.UseShellExecute = false;
-        //    processStartInfo.CreateNoWindow = true;
-        //    processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-        //    Process process = Process.Start(processStartInfo);
-        //    string error = process.StandardError.ReadToEnd();
-        //    process.WaitForExit();
-        //    if (process.ExitCode != 0)
-        //        throw new Exception(error);
-
-        //    var pdfContent = File.ReadAllText(@".\vtest.txt");
-        //    return pdfContent;
-        //}
     }
 }
--- test/xpdfexecutor.UnitTest/xpdfexecutorTest.cs
+++ test/xpdfexecutor.UnitTest/xpdfexecutorTest.cs
@@ -4,16 +4,6 @@ namespace xpdfexecutor.UnitTest
 {
     public class xpdfexecutorTest
     {
-        //[Fact]
-        //public void ShouldReturnXpdfVersionInfo()
-        //{
-        //    var xpdfexecutor = new Executor();
-
-        //    var actual = xpdfexecutor.GetVersionInfo();
-
-        //    Assert.StartsWith("pdftotext version 4.00", actual);
-        //}
-
         [Fact]
         public void ShouldReturnPdfContent()
         {
@@ -21,7 +11,7 @@ namespace xpdfexecutor.UnitTest
 
             var actual = xpdfexecutor.Execute();
 
-            Assert.Equal("This is first line!\r\n\f", actual);
+            Assert.Equal("This is first line!\r\n\r\n\f", actual);
         }
     }
 }
